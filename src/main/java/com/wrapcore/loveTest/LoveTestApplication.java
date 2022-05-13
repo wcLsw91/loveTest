34d411508476012dad8102098071242e4215f026
이성웅 <wc.lsw91@gmail.com>
2022-05-13 15:57:07 +0900

    Initial commit
    Signed-off-by: 이성웅 <wc.lsw91@gmail.com>

---
+++ src/main/java/com/wrapcore/loveTest/LoveTestApplication.java
@@ -0,0 +1,13 @@
+package com.wrapcore.loveTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wrapcore/loveTest/LoveTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrapcore.loveTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
