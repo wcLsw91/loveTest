8cec560c45afc0435c630648b74edc1cb2e45904
이성웅 <wc.lsw91@gmail.com>
2022-05-16 14:43:56 +0900

    디렉터리 구조 세팅

--- src/main/java/com/wrapcore/loveTest/_1web/MainController.java
+++ src/main/java/com/wrapcore/loveTest/_1web/MainController.java
@@ -1,4 +1,13 @@
 package com.wrapcore.loveTest._1web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
