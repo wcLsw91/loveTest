86cb073435b0ddece1fa6f5149f327bedff1518f
이성웅 <wc.lsw91@gmail.com>
2022-05-17 14:43:42 +0900

    로컬로 변경

--- src/main/java/com/wrapcore/loveTest/_1web/HomeController.java
+++ src/main/java/com/wrapcore/loveTest/_1web/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-public class MainController {
+public class HomeController {
 
     @GetMapping("/")
     @ResponseBody
--- src/main/java/com/wrapcore/loveTest/_3domain/question/Question.java
+++ src/main/java/com/wrapcore/loveTest/_3domain/question/Question.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "question_table")
+@Table(name = "question")
 public class Question {
 
     @Id
@@ -21,15 +21,15 @@ public class Question {
     @Column(name = "question_id")
     private Long id; // 문제 고유 번호
 
-    @Column(length = 50, nullable = false)
-    private String questionTitle; // 시험 주제 ex)연애고사, 전생테스트 등
-
     @Column(length = 20, nullable = false)
-    private Integer questionNumber; // 문제 번호
-    
-    @Column(length = 200, nullable = false)
-    private String questionContents; // 문제
-    
+    private String examSubject; // 시험 주제 ex)연애고사, 전생테스트 등
+
+    @Column(length = 3, nullable = false)
+    private Integer qNumber; // 문제 번호
+
+    @Column(length = 50, nullable = false)
+    private String question; // 문제
+
 //    private MultipartFile questionFile; //문제 이미지
 //    private String questionFilename; // 믄제 이미지 이름
 }
